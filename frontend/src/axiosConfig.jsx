4bb54866153d7719ff24e5a059f068857898e998
Natnicha B <nnatnichab@gmail.com>
2025-08-11 15:11:24 +1000

    Update IP address

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5001', // local
-  //baseURL: 'http://13.54.246.22:5001', // live
+  //baseURL: 'http://localhost:5001', // local
+  baseURL: 'http://3.27.183.81:5001', // live
   headers: { 'Content-Type': 'application/json' },
 });
 
