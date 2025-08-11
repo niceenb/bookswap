ba663dc6f6eb51a884649401924232820ec7d1db
Natnicha B <nnatnichab@gmail.com>
2025-08-12 09:27:02 +1000

    A1-32 Sub Task 4.2: Connect the viewing component to the backend API to fetch books

--- backend/models/Book.js
+++ backend/models/Book.js
@@ -6,6 +6,7 @@ const bookSchema = new mongoose.Schema({
   author: { type: String, required: true },
   genre: { type: String },
   publishedDate: { type: Date },
+  availability: { type: Boolean, default: true },
 });
 
 module.exports = mongoose.model('Book', bookSchema);
--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   //baseURL: 'http://localhost:5001', // local
-  baseURL: 'http://3.27.183.81:5001', // live
+  baseURL: 'http://16.176.169.3:5001', // live
   headers: { 'Content-Type': 'application/json' },
 });
 
