b14ea53de751f5ab0b875e4fa2cc253ac68926a2
Natnicha B <nnatnichab@gmail.com>
2025-08-11 14:46:38 +1000

    Show all book listing on home page

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  //baseURL: 'http://localhost:5001', // local
-  baseURL: 'http://13.238.120.40:5001', // live
+  baseURL: 'http://localhost:5001', // local
+  //baseURL: 'http://13.54.246.22:5001', // live
   headers: { 'Content-Type': 'application/json' },
 });
 
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,31 @@
+import { useAuth } from '../context/AuthContext';
+import { useState, useEffect } from 'react';
+import axiosInstance from '../axiosConfig';
+import AllBookList from '../components/AllBookList';
+
+const Home = () => {
+  const { user } = useAuth();
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    const fetchAllBooks = async () => {
+      try {
+        const response = await axiosInstance.get('/api/books/all');
+        setBooks(response.data);
+      } catch (error) {
+        alert('Could not load books.');
+      }
+    };
+
+    fetchAllBooks();
+  }, []);
+
+  return (
+    <div className="container mx-auto p-6">
+      <h1 className="text-2xl font-bold mb-4">All Books</h1>
+      <AllBookList books={books} userId={user?._id} />
+    </div>
+  );
+};
+
+export default Home;
