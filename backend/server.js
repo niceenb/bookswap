1a3a7a1835052c93f9e5734e7d82b6b12e2f1df1
Natnicha B <nnatnichab@gmail.com>
2025-08-15 09:09:28 +1000

    A1-39 Sub Task 8.2: Implement functionality to update book swap requests through the backend API

--- backend/server.js
+++ backend/server.js
@@ -14,6 +14,7 @@ app.use(express.json());
 app.use('/api/auth', require('./routes/authRoutes'));
 app.use('/api/tasks', require('./routes/taskRoutes'));
 app.use('/api/books', require('./routes/bookRoutes'));
+app.use('/api/swaps', require('./routes/swapRoutes'));
 
 
 
