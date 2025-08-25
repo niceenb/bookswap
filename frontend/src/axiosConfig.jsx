0d8dabdd952032b010acb0f29b77cc1ca8145c33
Natnicha B <nnatnichab@gmail.com>
2025-08-25 16:23:49 +1000

    Add comment

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -3,6 +3,7 @@ import axios from 'axios';
 const axiosInstance = axios.create({
   //baseURL: 'http://localhost:5001', // local
   baseURL: 'http://13.239.135.216:5001', // live
+  //add comment
   headers: { 'Content-Type': 'application/json' },
 });
 
